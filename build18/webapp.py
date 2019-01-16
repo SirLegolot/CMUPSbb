91e7d1834d4dbfc8ce6a2d6d60ca4155fe0341e5
SirLegolot <43505146+SirLegolot@users.noreply.github.com>
2019-01-16 16:46:55 -0500

    Update webapp.py

--- build18/webapp.py
+++ build18/webapp.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 import os
+import sqlite3
 
 
 app = Flask(__name__)
 
-fields = ["SMC: ", "First name: ", "Last name: "]
+fields = ["andrewID: ", "SMC: ", "Last name: "]
 filename = "testing shit.txt"
+
+def connect(database):
+	conn = sqlite3.connect(database)
+	return conn
 
 @app.route('/')
 def root():
@@ -13,15 +18,25 @@ def root():
  
 @app.route('/submitted')
 def poll():
+    andrew = request.args.get("andrewID: ")
     smc = request.args.get("SMC: ")
-    firstname = request.args.get("First name: ")
     lastname = request.args.get("Last name: ")
+
+    conn = connect("data.db")
+    c = conn.cursor()
+    c.execute("""SELECT bin FROM packages WHERE andrew = ? WHERE smc = ? WHERE lastname = ?""",(andrew,smc,lastname,))
+    result = c.fetchone()
+    if len(result)>0:
+    	pass
+
 
     out = open(filename, 'a')
     out.write(smc+" "+firstname+" "+lastname+"\n")
     out.close()
 
+
     return "Thanks! You are now in the queue."
+
 
 
 if __name__ == "__main__":
