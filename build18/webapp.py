d03a9891bdbfa7a99d437a3048fdf8eeffd5e18e
SirLegolot <43505146+SirLegolot@users.noreply.github.com>
2019-01-17 19:22:26 -0500

    updated slightly-- NO LONGER BEING USED

--- build18/webapp.py
+++ build18/webapp.py
@@ -6,7 +6,7 @@ import sqlite3
 app = Flask(__name__)
 
 fields = ["andrewID: ", "SMC: ", "Last name: "]
-filename = "testing shit.txt"
+filename = "queue.txt"
 
 def connect(database):
 	conn = sqlite3.connect(database)
@@ -18,16 +18,16 @@ def root():
  
 @app.route('/submitted')
 def poll():
-	andrew = request.args.get("andrewID: ")
+	andrew = request.args.get("andrewID: ").lower()
 	smc = request.args.get("SMC: ")
-	lastname = request.args.get("Last name: ")
+	lastname = request.args.get("Last name: ").lower()
 	conn = connect("data.db")
 	c = conn.cursor()
 	c.execute("""SELECT bin FROM packages WHERE andrew = ? AND smc = ? AND lastname = ?""",(andrew,smc,lastname,))
-	result = c.fetchone()
-	if result!=None:
+	result = c.fetchall()
+	if len(result)!=0:
 		out = open(filename, 'a')
-		out.write(smc+" "+firstname+" "+lastname+"\n")
+		out.write(smc+" "+andrew+" "+lastname+"\n")
 		out.close()
 		return "Thanks! You are now in the queue. You have "+str(len(result))+"packages."
 	else:
